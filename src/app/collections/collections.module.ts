005eb57438401e06b75c804e3d556e6663ba6d6d
Azhar <azharibraheem482@gmail.com>
2023-12-21 11:58:11 +0530

    first commit

---
+++ src/app/collections/collections.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsRoutingModule } from './collections-routing.module';
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+
+@NgModule({
+  declarations: [
+    CollectionsHomeComponent
+  ],
+  exports: [
+
+  ],
+  imports: [
+    CommonModule,
+    CollectionsRoutingModule
+  ]
+})
+export class CollectionsModule { }
